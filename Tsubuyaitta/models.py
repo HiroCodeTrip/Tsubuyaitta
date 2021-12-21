ed77a3d2307ade743924881791585f84adeb08ef
s1f102103819 <85655907+s1f102103819@users.noreply.github.com>
2021-12-21 20:41:52 +0900

    change

--- Tsubuyaitta/models.py
+++ Tsubuyaitta/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.db.models.fields import TextField
+from django.utils import timezone
 
 # Create your models here.
+
+class Question(models.Model):
+
+    class Meta:
+        db_table = "Question"
+
+    comment = models.CharField(verbose_name="コメント",max_length=200)
+
+    def __str__(self):
+        return self.comment
+
+class Question(models.Model):
+    id = models.CharField(max_length=15)
+    content = models.TextField()
+    posted_at = models.DateTimeField(default=timezone.now)
+    published_at =models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        self.published_at = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.id
+
--- Tsubuyaitta/views.py
+++ Tsubuyaitta/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
-# Create your views here.
+from django.views import View
+from .models import Question
+
+class TsubuView(View):
+
+    def get(self, request, *args, **kwargs):
+
+        questions  = Question.objects.all()
+        context = { "questions":questions }
+
+        return render(request,"Tsubu/index.html",context)
+
+    def post(self, request, *args, **kwargs):
+
+        posted  = Question( comment = request.POST["comment"] )
+        posted.save()
+
+        return redirect("Tsubu:index")
+
+index   = TsubuView.as_view()
+
+def index(request):
+    if request.method == 'POST':
+        question = Question(id=request.POST['id'], content=request.POST['text'])
+        question.save()
+        return redirect()
