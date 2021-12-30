6892c956afe626b715de868801fbecc5280abb5c
Hiro <s1f102103561@iniad.org>
2021-12-30 15:07:42 +0900

    とりあえず表示

--- Tsubuyaitter/urls.py
+++ Tsubuyaitter/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from . import views
+from django.views.generic import TemplateView
 
 #''/'' now
 urlpatterns = [
-    path('', views.index, name='t_index'),
-    path('<int:room_id>/', views.room, name='room')
+    path('',TemplateView.as_view(template_name="tsubuyaitter/appex.html"),name='appex'),
+
+
+    #path('', views.index, name='t_index'),
+    #path('<int:room_id>/', views.room, name='room')
 ]
