f888aa1a025be885200db3cdf551e8ba918119e2
s1F10210357 <85667226+s1F10210357@users.noreply.github.com>
2022-01-04 22:43:56 +0900

    fix about room making page
    ユーザーがstaff以上の場合にのみ表示されるリンクからroom作成を可能にしました。
    また、手元では以下は確認済みです。
    ・staffでのルーム作成と前後の挙動、DBへの反映
    ・activeではリンクが表示されないこと

--- Tsubuyaitter/urls.py
+++ Tsubuyaitter/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('signup/',views.signup,name='signup'),
     path('login/',auth_views.LoginView.as_view(template_name="tsubuyaitter/login.html"),name='login'),
     path('logout/',auth_views.LogoutView.as_view(template_name="tsubuyaitter/logout.html"),name='logout'),
-    path('home',views.home, name='home')
+    path('home',views.home, name='home'),
     #path('', views.index, name='t_index'),
-    #path('<int:room_id>/', views.room, name='room')
+    path('<int:room_id>/', views.room, name='room'),
 ]
