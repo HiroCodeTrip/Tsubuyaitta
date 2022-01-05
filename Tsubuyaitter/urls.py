bd2e4157594e066b4f04b7d8c48d6b65b3969039
Hiro <s1f102103561@iniad.org>
2022-01-05 15:42:33 +0900

    logoutなんかいらん

--- Tsubuyaitter/urls.py
+++ Tsubuyaitter/urls.py
@@ -8,7 +8,6 @@ urlpatterns = [
     path('',TemplateView.as_view(template_name="tsubuyaitter/appex.html"),name='appex'),
     path('signup/',views.signup,name='signup'),
     path('login/',auth_views.LoginView.as_view(template_name="tsubuyaitter/login.html"),name='login'),
-    path('logout/',auth_views.LogoutView.as_view(template_name="tsubuyaitter/logout.html"),name='logout'),
     path('home',views.home, name='home'),
     #path('', views.index, name='t_index'),
     path('<int:room_id>/', views.room, name='room'),
